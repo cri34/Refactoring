f65c1da22e3333c39939e11f5a5a0d77ed7d593e
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-05-28 17:32:10 +0200

    04-ExtractMethod finalizado

--- 04-extract-Method/UrlNormalizer.java
+++ 04-extract-Method/UrlNormalizer.java
@@ -7,8 +7,19 @@ public class UrlNormalizer {
 		String url = "";
 		// First we trim whitespaces
 		url = title.trim();
-
 		// Remove special chars
+		url = removeSpecialChars();
+
+		// Replace white spaces with hyphens
+		
+		url = remplazeWhiteSpaces();
+
+		// lowercase everything
+		url = url.toLowerCase();
+
+		return url;
+	}
+	private String removeSpecialChars (){
 		String specialRemoved = "";
 		for (int i = 0; i < url.length(); i++) {
 			if (url.charAt(i) != ',' && url.charAt(i) != ':' 
@@ -16,11 +27,11 @@ public class UrlNormalizer {
 				specialRemoved += url.charAt(i);
 			}
 		}
-
-		url = specialRemoved;
-
-		// Replace white spaces with hyphens
-		String spacesReplaced = "";
+		return specialRemoved;
+	}
+	
+	private String remplazeWhiteSpaces(){
+String spacesReplaced = "";
 		for (int i = 0; i < url.length(); i++) {
 			if (url.charAt(i) == ' ') {
 				spacesReplaced += "-";
@@ -28,12 +39,7 @@ public class UrlNormalizer {
 				spacesReplaced += url.charAt(i);
 			}
 		}
-		url = spacesReplaced;
-
-		// lowercase everything
-		url = url.toLowerCase();
-
-		return url;
+		return spacesReplaced;
 	}
 }
 
