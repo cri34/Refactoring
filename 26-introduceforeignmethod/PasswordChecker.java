ec977e6dd5936cba3d51543061d24722375db246
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-06-01 15:59:43 +0200

    26-IntroduceForeignMethod finalizado

--- 26-introduceforeignmethod/PasswordChecker.java
+++ 26-introduceforeignmethod/PasswordChecker.java
@@ -2,10 +2,14 @@ package refactoritzacions;
 
 public class PasswordChecker {
 	public static String improvePassword (String password) {
-		if (password.length() < 5) {
-			return "****" + password + "****";
-		} else {
-			return password;
+		int minPasswordLenght=5;
+		if (password.length() < minPasswordLenght) {
+			return makeValidPassword(password);
 		}
+			return password;
+	}
+	private String makeValidPassword(String password){
+		return "****" + password + "****";
+
 	}
 }
